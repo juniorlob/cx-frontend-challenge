f7a83b5e65d2ec1e2c2e64eaf77b48a47db2f7c7
Junior Lobo <jairraiollobo@gmail.com>
2023-12-18 20:11:54 -0300

    feat: Implement Input component

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,4 +1,7 @@
-import '@/styles/globals.css'
+import '@/lib/styles/globals.css'
+import '@/lib/styles/variables.css'
+import '@/lib/styles/icons.css'
+
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,11 +1,65 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import styles from '@/lib/styles/home.module.css'
+import Header from '@/lib/components/shared/header'
+import { Input } from '@/lib/components/shared'
+import ProductCard from '@/lib/components/shared/product-card'
+import { useEffect, useState } from 'react'
+import { SearchResult } from '@/lib/models/search.model'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const objectToURLParams = (object: any) => {
+  return Object.entries(object)
+    .map(([key, value]) => `${key}=${value}`)
+    .join('&')
+}
+
+const API_ROUTE = 'https://api.mercadolibre.com/sites/MLA/search'
+interface Filters {
+  [key: string]: string
+}
+const queryParams = new Map(
+  [
+    ['q', 'milho'],
+    ['order', 'desc'],
+    ['sort', 'price'],
+    ['limit', 10],
+  ].map(([key, value]) => [key, value])
+)
+const apiRoute = (route: string, params: Map<string, string | number>) => {
+  const url = new URL(route)
+  const urlParams = new URLSearchParams(params)
+  url.search = urlParams.toString()
+  return url.href
+}
+
 export default function Home() {
+  const [products, setProducts] = useState<SearchResult | null>()
+
+  const [filters, setFilters] =
+    useState<Map<string, string | number>>(queryParams)
+
+  const onFiltersChange = (filter) => {
+    const [[param, value]] = Object.entries(filter)
+
+    setFilters((prev) => new Map([...prev]).set(param, value))
+  }
+
+  useEffect(() => {
+    ;(async function () {
+      try {
+        const response = await fetch(apiRoute(API_ROUTE, filters))
+        const data = await response.json()
+        const searchResults = new SearchResult(data)
+
+        setProducts(searchResults)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    })()
+  }, [filters])
   return (
     <>
       <Head>
@@ -15,108 +69,14 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
+        <Header>
+          <Input name="q" endAdornment onChange={onFiltersChange} />
+        </Header>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+        {products?.results?.size > 0 &&
+          Array.from(products.results.entries()).map(([id, product]) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
       </main>
     </>
   )
